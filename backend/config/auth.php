819cdb8908d8ecc1a6cf288119a51aef67d7dfe4
saxsir <saxsir.256+github@gmail.com>
2021-02-21 17:05:48 +0900

    Change driver eloquent => auth0

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -67,8 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'driver' => 'auth0',
         ],
 
         // 'users' => [
